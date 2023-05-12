faed980aa950f29bd8bca6695e68b70ab6905267
GustavoVFA <gustavovfamorim@gmail.com>
2023-05-11 21:13:42 -0300

    delay SplashActivity

--- app/src/main/java/br/com/etecia/myapp/SplashActivity.java
+++ app/src/main/java/br/com/etecia/myapp/SplashActivity.java
@@ -1,8 +1,11 @@
 package br.com.etecia.myapp;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_layout);
+
+        // Carregamento da tela de login
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+                finish();
+            }
+        },3000);
     }
 }
